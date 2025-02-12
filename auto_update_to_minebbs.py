7a776a8e7715a149e6a1cb8688181ab5955ad4ee
LT_Name <572413378@qq.com>
2025-02-12 21:37:13 +0800

    feat: 添加配置文件支持及DeepSeek翻译功能
    - 新增 `config.json` 文件用于存储配置信息
    - 重构代码以支持从配置文件读取 GitHub、Minebbs 和 DeepSeek 的配置
    - 添加 DeepSeek API 调用以实现提交信息的翻译功能

--- auto_update_to_minebbs.py
+++ auto_update_to_minebbs.py
@@ -3,39 +3,52 @@ import os
 
 import requests
 
-# GitHub 仓库名称
-repo_owner = 'CloudburstMC'
-repo_name = 'Nukkit'
+# 工作路径 用于存储配置文件等
+# 如果出现获取不到配置文件的情况，请手动设置为脚本所在路径
+work_file_dir = os.getcwd()
 
-# GitHub API URL
-github_url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/commits'
-
-# minebbs的token
-minebbs_token = ''
-# minebbs资源id
-minebbs_id = '32'
-# minebbs资源下载地址
-file_url = "https://ci.opencollab.dev/job/NukkitX/job/Nukkit/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar"
-
-minebbs_api_url = f'https://api.minebbs.com/api/openapi/v1/resources/{minebbs_id}/update'
-
-# 缓存文件路径
-cache_file_dir = os.getcwd()
+def load_config(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
 
 def save_last_commit_id(commit_id):
-    with open(f'{cache_file_dir}/last_commit_id.txt', 'w') as f:
+    with open(f'{work_file_dir}/last_commit_id.txt', 'w') as f:
         f.write(commit_id)
 
+
 def get_last_commit_id():
-    with open(f'{cache_file_dir}/last_commit_id.txt', 'r') as f:
-        return f.read()
+    try:
+        with open(f'{work_file_dir}/last_commit_id.txt', 'r') as f:
+            return f.read()
+    except FileNotFoundError:
+        return ""
+
+def translate_commit_message(key, msg):
+    if len(key) == 0 or len(msg) == 0:
+        return "翻译错误"
+    from openai import OpenAI
+    client = OpenAI(api_key=key, base_url="https://api.deepseek.com")
+
+    res = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {"role": "system", "content": "翻译中文到英文，其他语言全部翻译为中文，只需要回复翻译后的语句"},
+            {"role": "user", "content": msg},
+        ],
+        stream=False
+    )
+
+    return res.choices[0].message.content
+
 
 if __name__ == '__main__':
-    response = requests.get(github_url)
+    config = load_config(f'{work_file_dir}/config.json')
+
+    response = requests.get(f"https://api.github.com/repos/{config['github']['repo_owner']}/{config['github']['repo_name']}/commits")
 
     if response.status_code == 200:
         commits = response.json()
-        commit = commits[0] #只获取最新的一次提交
+        commit = commits[0]  # 只获取最新的一次提交
 
         sha = commit['sha']
         message = commit['commit']['message']
@@ -45,23 +58,40 @@ if __name__ == '__main__':
             exit(0)
 
         headers = {
-            'Authorization': f'Bearer {minebbs_token}',
+            'Content-Type': 'application/json',
+            'Authorization': f"Bearer {config['minebbs']['token']}",
         }
+
+        # 翻译
+        deepseek_api_key = config['deepseek']['token']
+        if len(deepseek_api_key) > 0:
+            message = (f"{message}\n\n"
+                       f"以下为机器翻译，请注意内容的准确性\n"
+                       f"{translate_commit_message(deepseek_api_key, message)}")
 
         data = {
             'title': f'master-{sha[:7]}',
             'description': message,
             'new_version': f'master-{sha[:7]}',
-            'file_url': file_url
+            'file_url': config['minebbs']['res_file_url']
         }
 
-        response = requests.post(minebbs_api_url, headers=headers, data=json.dumps(data))
+        try:
+            response = requests.post(
+                f"https://api.minebbs.com/api/openapi/v1/resources/{config['minebbs']['res_id']}/update",
+                headers=headers,
+                data=json.dumps(data),
+                verify=False
+            )
 
-        if response.status_code == 200:
-            print('Uploaded successfully')
+            if response.status_code == 200:
+                print('Uploaded successfully')
+                save_last_commit_id(sha)
+            else:
+                print(f'Error uploading: {response.status_code} {response.text}')
+        except Exception as e:
             save_last_commit_id(sha)
-        else:
-            print(f'Error uploading: {response.status_code} {response.text}')
+            print(e)
     else:
         print(f'Error fetching commits: {response.status_code}')
 
